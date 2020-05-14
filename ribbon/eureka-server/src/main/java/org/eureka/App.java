b7bf8f5b45d035d4cc9bfa41052a5aae26ce83b8
satish <satishveluru@gmail.com>
2020-05-14 01:16:29 -0400

    Initial Multi Module Project

---
+++ ribbon/eureka-server/src/main/java/org/eureka/App.java
@@ -0,0 +1,13 @@
+package org.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
